5ab1426dd5933fe4be1064a0132417131ebf29a9
Anthony Gaimari <tgaimari@comcast.net>
2019-06-20 13:44:48 -0500

    added pages and update

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE chirps (
+CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(45) NULL,
   devoured BOOLEAN NOT NULL DEFAULT 0,
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whopper with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
